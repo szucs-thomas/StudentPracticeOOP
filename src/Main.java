18110afcb314bed63c457d7feb23e36fa74db331
Tom <szucs.thomas1987@gmail.com>
2023-09-02 07:09:05 +0200

    Simple Practice, second plan.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,93 @@
+import java.util.Scanner;
+
 public class Main {
+    public static int beker(String szoveg) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(szoveg);
+        return sc.nextInt();
+    }
+
+    public static String szovegbBeker(String szoveg) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(szoveg);
+        return sc.nextLine();
+    }
+    public static double atlag (Diak[] tomb) {
+        int osszeg = 0;
+        for (Diak d : tomb) {
+            osszeg+= d.getJegy();
+        }
+        return (double) osszeg/ tomb.length;
+    }
+    public static void atlagnalJobb(Diak[] tomb) {
+        double atlag = atlag(tomb);
+        for (Diak d : tomb) {
+            if (d.getJegy()>atlag) {
+                d.kiir();
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        Diak[] diakok = new Diak[9];
-        int osszLetszam = 0;
+        int osszLetszam = beker("Kérem adja meg az össz létszámot: ");
+        int infoLetszam = beker("Kérem adja meg az Informatikai osztály létszámát: ");
+        int jogLetszam = beker("Kérem adja meg a Jog osztály létszámát: ");
+        int fizLetszam = beker("Kérem adja meg a Fizika osztály létszámát: ");
+        int maxPont = beker("Kérem adja meg a max pontszámot: ");
 
-        diakok[0]=new Diak("Szucs Tamas","Szolnok, Lovas Istvan utca 3",89,"8.A",100);
-        diakok[1]=new Diak("Szucs Tibor","Szolnok, Bercsenyi ut 32",68,"7.B",100);
-        diakok[2]=new Diak("Kovacs Peter","Budapest,Robert Karoly krt. 56",36,"11.T",100);
-        diakok[3]= new InformatikusDiak("Nagy Imre","Budapest, Varkonyi ter 6.",56,"9.B",100,"C#",4);
-        diakok[4] = new InformatikusDiak("Karoly Bela","Veszprem, Kolzsvari 89",83,"10.C",100,"Java",5);
-        diakok[5]= new JogaszDiak("Herczeg Andras","Bekescsaba, Martirok utja 34",50,"7.A",100,"Romai Jog","Werboczi kollegium");
-        diakok[6] = new JogaszDiak("Nagy Karoly","Sopron, Petofi setany 6.",19,"12.T",100,"Filozofia","Seprenczi kollegium");
-        diakok[7]= new FizikaDiak("Kis Armand","Debrecen, Petofi setany 38.",69,"13.A",100,"Stephen Hawking",13);
-        diakok[8]= new FizikaDiak("Cserepes Klara","Gyula, Kastely krt. 48.",78,"6.B",100,"Teller Ede",23);
+        Diak[] diakok = new Diak[osszLetszam];
 
+        int flag = 0;
+
+        System.out.println("Informatika Osztály: ");
+        for (int i = 0; i < infoLetszam; i++) {
+            String nev = szovegbBeker("Kérem adja meg a(z) " + (flag + 1) + ". diák nevét: ");
+            String cim = szovegbBeker("Kérem adja meg a " + nev + " diák címét: ");
+            int elertPont = beker("Kérem a " + nev + " Elért pontját: ");
+            String osztaly = szovegbBeker("Kérem adja meg a " + nev + " diák osztályát: ");
+            String kedvencNyelv = szovegbBeker("Kérem adja meg kedvenc program nyelvét: ");
+            int tanultProgEv = beker("Kérem adja meg hány évig programozott: ");
+
+            diakok[flag] = new InformatikusDiak(nev, cim, elertPont, osztaly, maxPont, kedvencNyelv, tanultProgEv);
+            flag++;
+        }
+
+        System.out.println("Jogi Osztály: ");
+        for (int i = 0; i < jogLetszam; i++) {
+            String nev = szovegbBeker("Kérem adja meg a(z) " + (flag + 1) + ". diák nevét: ");
+            String cim = szovegbBeker("Kérem adja meg a " + nev + " diák címét: ");
+            int elertPont = beker("Kérem a " + nev + " Elért pontját: ");
+            String osztaly = szovegbBeker("Kérem adja meg a " + nev + " diák osztályát: ");
+            String kedvencTargy = szovegbBeker("Kérem adja meg a kedvenc tantárgyát: ");
+            String kollegiumNeve = szovegbBeker("Kérem adja meg a kollégiuma nevét: ");
+
+            diakok[flag] = new JogaszDiak(nev, cim, elertPont, osztaly, maxPont, kedvencTargy, kollegiumNeve);
+            flag++;
+        }
+
+        System.out.println("Fizika Osztály: ");
+        for (int i = 0; i < fizLetszam; i++) {
+            String nev = szovegbBeker("Kérem adja meg a(z) " + (flag + 1) + ". diák nevét: ");
+            String cim = szovegbBeker("Kérem adja meg a " + nev + " diák címét: ");
+            int elertPont = beker("Kérem a " + nev + " Elért pontját: ");
+            String osztaly = szovegbBeker("Kérem adja meg a " + nev + " diák osztályát: ");
+            String kedvencFizikus = szovegbBeker("Kérem adja meg kedvenc Fizikusát: ");
+            int kiserletekSzama = beker("Kérem adja meg kísérleteinek számát: ");
+
+            diakok[flag] = new FizikaDiak(nev, cim, elertPont, osztaly, maxPont, kedvencFizikus, kiserletekSzama);
+            flag++;
+
+        }
+        System.out.println();
+        System.out.println("A diákok eredményei: ");
         for (Diak diak : diakok) {
             diak.kiir();
         }
+        System.out.print("Az osztály átlaga: "+atlag(diakok));
+        System.out.println();
+        System.out.println("Az átlagnál jobb diákok: ");
+        atlagnalJobb(diakok);
+
 
     }
 }
